adb3673e4e1b85421b145e453aa49066717ca9e4
Mayorkun <samuelayanshina@gmail.com>
2025-06-21 10:49:27 +0100

    Initial Commit

---
+++ CardStackAnimation/CardStackAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardStackAnimationApp.swift
+//  CardStackAnimation
+//
+//  Created by USER on 6/21/25.
+//
+
+import SwiftUI
+
+@main
+struct CardStackAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardStackAnimationTests/CardStackAnimationTests.swift
@@ -0,0 +1,17 @@
+//
+//  CardStackAnimationTests.swift
+//  CardStackAnimationTests
+//
+//  Created by USER on 6/21/25.
+//
+
+import Testing
+@testable import CardStackAnimation
+
+struct CardStackAnimationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
